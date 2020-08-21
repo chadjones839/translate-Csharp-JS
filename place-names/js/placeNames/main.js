4dec3f5ecb3530ea6f63ae90342645d92ebf35d7
Chad Jones <chadjones839@gmail.com>
2020-08-21 11:54:54 -0500

    place names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,22 @@
 function main() {
-  // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+  console.log("All Place Names", names);
+  console.log()
+
+  console.log("Each Name:")
+  names.forEach(name => {
+    console.log(name)
+  })
+  console.log()
+
+  const theNames = names.filter((name) => name.startsWith("The"));
+
+  console.log("'The' Place Names")
+  theNames.forEach(name => {
+    console.log(name)
+  })
 
 }
 
